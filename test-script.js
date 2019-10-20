6f13723f956f0990bf80e45f8f639eb3f2450825
leraNelepina <56783678+leraNelepina@users.noreply.github.com>
2019-10-20 12:13:38 +0500

    Add files via upload

---
+++ test-script.js
@@ -0,0 +1,13 @@
+function down()
+{
+	var a = document.getElementById("dropdown")
+	
+	if (a.style.display == "none")
+	{
+		a.style.display = "block";
+	}
+	else
+	{
+		a.style.display = "none";
+	}
+}
